f0c215d55fbaa2a14c9059843709c1aefe4cf22b
FearlessAdam <144472440+FearlessAdam@users.noreply.github.com>
2024-03-25 13:31:10 +0530

    Added new Features

--- main.py
+++ main.py
@@ -25,6 +25,13 @@ input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def DoLoad():
     global noDo, randNum1, randdiv, mkae1
+    microVersion = control.hardware_version()
+    if microVersion == "1":
+        basic.show_string("Incompatible MicroBit!")
+        basic.pause(4000)
+        control.panic(668)
+    bluetooth.start_uart_service()
+    bluetooth.set_transmit_power(8)
     serial.set_baud_rate(BaudRate.BAUD_RATE14400)
     music.play(music.builtin_playable_sound_effect(soundExpression.giggle),
         music.PlaybackMode.LOOPING_IN_BACKGROUND)
@@ -39,6 +46,7 @@ def DoLoad():
         # . . . .
         # . . . .
         """)
+    serial.write_line("Starting bluetooth..")
     basic.pause(randdiv / 2.5)
     basic.show_leds("""
         # # . . .
@@ -55,6 +63,9 @@ def DoLoad():
         # # # . .
         # # # . .
         """)
+    serial.write_line("Loading Modules..")
+    basic.pause(900)
+    serial.write_line("ModuleLoad: Done")
     basic.pause(randdiv / 2.5)
     basic.show_leds("""
         # # # # .
@@ -71,8 +82,10 @@ def DoLoad():
         # # # # #
         # # # # #
         """)
+    serial.write_line("Setting Transmit Power..")
     basic.show_icon(IconNames.YES)
     mkae1 = randdiv / 2.5 * 4
+    serial.write_line("Finalizing Boot..")
     serial.write_line("Took " + ("" + str(mkae1)) + " ms to load")
     serial.write_line("----------------------------------------------------------")
     serial.write_line("MicroWatch 1.2/Beta Running on Micro:bit \"" + control.device_name() + "\"")
@@ -148,6 +161,10 @@ buttonClicks.on_button_held(buttonClicks.AorB.A, my_function2)
 
 runningstatus = 0
 xAcceleration = 0
+BleBSubNum = 0
+BleB = ""
+BleASubNum = 0
+bleA = ""
 scores = ""
 time = ""
 minute = 0
@@ -160,13 +177,13 @@ newly1 = 0
 gameoption = 0
 astroid2: game.LedSprite = None
 noDo = 0
-randdiv2 = 0
-randNum12 = 0
-noDo2 = 0
-second = 0
-laser: game.LedSprite = None
-asteroid: game.LedSprite = None
 player: game.LedSprite = None
+asteroid: game.LedSprite = None
+laser: game.LedSprite = None
+second = 0
+noDo2 = 0
+randNum12 = 0
+randdiv2 = 0
 DoLoad()
 delay = 500
 
@@ -175,9 +192,166 @@ def on_every_interval():
     minute += 1
 loops.every_interval(60000, on_every_interval)
 
+def on_forever():
+    global scores
+    if noDo == 0:
+        scores = "Score:" + ("" + str(game.score()))
+basic.forever(on_forever)
+
+def on_forever2():
+    global hour, minute, time
+    if noDo == 0:
+        if hour == 24:
+            hour = 0
+        elif minute == 60:
+            minute = 0
+            hour += 1
+    time = "" + str(hour) + ":" + ("" + str(minute))
+basic.forever(on_forever2)
+
+def on_forever3():
+    global bleA, BleASubNum, hour
+    bleA = bluetooth.uart_read_until(serial.delimiters(Delimiters.NEW_LINE))
+    BleASubNum = parse_float(bleA.substr(5, 8))
+    if bleA.includes("Hour="):
+        if BleASubNum < 25:
+            hour = BleASubNum
+            serial.write_line("The Hour is set to " + str(hour) + " Via Bluetooth serial Input.")
+basic.forever(on_forever3)
+
+def on_forever4():
+    global BleB, BleBSubNum, minute
+    BleB = bluetooth.uart_read_until(serial.delimiters(Delimiters.NEW_LINE))
+    BleBSubNum = parse_float(BleB.substr(4, 7))
+    if BleB.includes("Min="):
+        if BleBSubNum < 61:
+            minute = BleBSubNum
+            serial.write_line("The Minute is set to " + str(minute) + " Via Bluetooth serial Input.")
+basic.forever(on_forever4)
+
+def on_forever5():
+    global player
+    if noDo == 0:
+        if gameoption == 24:
+            player = game.create_sprite(2, 4)
+basic.forever(on_forever5)
+
+def on_forever6():
+    global xAcceleration, noexecute
+    if noDo == 0:
+        if gameoption == 24:
+            xAcceleration = input.acceleration(Dimension.X)
+            if xAcceleration < -150 and player.get(LedSpriteProperty.X) > 0:
+                player.change(LedSpriteProperty.X, -1)
+            elif xAcceleration > 150 and player.get(LedSpriteProperty.X) < 4:
+                player.change(LedSpriteProperty.X, 1)
+            basic.pause(50)
+        elif gameoption == 12:
+            xAcceleration = input.acceleration(Dimension.X)
+            if xAcceleration < -150 and player.get(LedSpriteProperty.X) > 0:
+                player.change(LedSpriteProperty.X, -1)
+            elif xAcceleration > 150 and player.get(LedSpriteProperty.X) < 4:
+                player.change(LedSpriteProperty.X, 1)
+            basic.pause(50)
+        else:
+            noexecute = 12
+basic.forever(on_forever6)
+
+# asteroid movement
+
+def on_forever7():
+    global astroid2, gameoption, newly1
+    if noDo == 0:
+        if gameoption == 24:
+            astroid2 = game.create_sprite(Math.random_range(0, 5), 0)
+            while astroid2.get(LedSpriteProperty.Y) < 4:
+                astroid2.change(LedSpriteProperty.Y, 1)
+                basic.pause(delay)
+                if laser != None and laser.is_touching(astroid2):
+                    astroid2.delete()
+                    laser.delete()
+            if player.is_touching(astroid2):
+                gameoption = 0
+                newly1 = 0
+                game.set_score(0)
+                game.pause()
+                asteroid.delete()
+                astroid2.delete()
+                player.delete()
+                basic.clear_screen()
+            else:
+                astroid2.delete()
+                game.add_score(1)
+        elif gameoption == 12:
+            astroid2 = game.create_sprite(Math.random_range(0, 5), 0)
+            while astroid2.get(LedSpriteProperty.Y) < 4:
+                astroid2.change(LedSpriteProperty.Y, 1)
+                basic.pause(delay)
+                if laser != None and laser.is_touching(astroid2):
+                    astroid2.delete()
+                    laser.delete()
+            if player.is_touching(astroid2):
+                gameoption = 0
+                newly1 = 0
+                game.set_score(0)
+                game.pause()
+                asteroid.delete()
+                astroid2.delete()
+                player.delete()
+                basic.clear_screen()
+            else:
+                astroid2.delete()
+                game.add_score(1)
+basic.forever(on_forever7)
+
+def on_forever8():
+    global asteroid, gameoption, newly1
+    if noDo == 0:
+        if gameoption == 24:
+            asteroid = game.create_sprite(Math.random_range(0, 5), 0)
+            while asteroid.get(LedSpriteProperty.Y) < 4:
+                asteroid.change(LedSpriteProperty.Y, 1)
+                basic.pause(delay)
+                if laser != None and laser.is_touching(asteroid):
+                    asteroid.delete()
+                    laser.delete()
+            if player.is_touching(asteroid):
+                gameoption = 0
+                newly1 = 0
+                game.set_score(0)
+                game.pause()
+                asteroid.delete()
+                astroid2.delete()
+                player.delete()
+                basic.clear_screen()
+            else:
+                asteroid.delete()
+                game.add_score(1)
+        elif gameoption == 12:
+            asteroid = game.create_sprite(Math.random_range(0, 5), 0)
+            while asteroid.get(LedSpriteProperty.Y) < 4:
+                asteroid.change(LedSpriteProperty.Y, 1)
+                basic.pause(delay)
+                if laser != None and laser.is_touching(asteroid):
+                    asteroid.delete()
+                    laser.delete()
+            if player.is_touching(asteroid):
+                gameoption = 1
+                newly1 = 0
+                game.set_score(0)
+                game.pause()
+                asteroid.delete()
+                astroid2.delete()
+                player.delete()
+                basic.clear_screen()
+            else:
+                asteroid.delete()
+                game.add_score(1)
+basic.forever(on_forever8)
+
 # firing lasers
 
-def on_forever():
+def on_forever9():
     global noexecute
     if noDo == 0:
         if gameoption == 24:
@@ -212,141 +386,7 @@ def on_forever():
             
         else:
             noexecute = 8
-basic.forever(on_forever)
-
-def on_forever2():
-    global scores
-    if noDo == 0:
-        scores = "Score:" + ("" + str(game.score()))
-basic.forever(on_forever2)
-
-def on_forever3():
-    global hour, minute, time
-    if noDo == 0:
-        if hour == 24:
-            hour = 0
-        elif minute == 60:
-            minute = 0
-        time = "" + str(hour) + ":" + ("" + str(minute))
-basic.forever(on_forever3)
-
-def on_forever4():
-    global xAcceleration, noexecute
-    if noDo == 0:
-        if gameoption == 24:
-            xAcceleration = input.acceleration(Dimension.X)
-            if xAcceleration < -150 and player.get(LedSpriteProperty.X) > 0:
-                player.change(LedSpriteProperty.X, -1)
-            elif xAcceleration > 150 and player.get(LedSpriteProperty.X) < 4:
-                player.change(LedSpriteProperty.X, 1)
-            basic.pause(50)
-        elif gameoption == 12:
-            xAcceleration = input.acceleration(Dimension.X)
-            if xAcceleration < -150 and player.get(LedSpriteProperty.X) > 0:
-                player.change(LedSpriteProperty.X, -1)
-            elif xAcceleration > 150 and player.get(LedSpriteProperty.X) < 4:
-                player.change(LedSpriteProperty.X, 1)
-            basic.pause(50)
-        else:
-            noexecute = 12
-basic.forever(on_forever4)
-
-def on_forever5():
-    global player
-    if noDo == 0:
-        if gameoption == 24:
-            player = game.create_sprite(2, 4)
-basic.forever(on_forever5)
-
-# asteroid movement
-
-def on_forever6():
-    global astroid2, gameoption, newly1
-    if gameoption == 24:
-        astroid2 = game.create_sprite(Math.random_range(0, 5), 0)
-        while astroid2.get(LedSpriteProperty.Y) < 4:
-            astroid2.change(LedSpriteProperty.Y, 1)
-            basic.pause(delay)
-            if laser != None and laser.is_touching(astroid2):
-                astroid2.delete()
-                laser.delete()
-        if player.is_touching(astroid2):
-            gameoption = 0
-            newly1 = 0
-            game.set_score(0)
-            game.pause()
-            asteroid.delete()
-            astroid2.delete()
-            player.delete()
-            basic.clear_screen()
-        else:
-            astroid2.delete()
-            game.add_score(1)
-    elif gameoption == 12:
-        astroid2 = game.create_sprite(Math.random_range(0, 5), 0)
-        while astroid2.get(LedSpriteProperty.Y) < 4:
-            astroid2.change(LedSpriteProperty.Y, 1)
-            basic.pause(delay)
-            if laser != None and laser.is_touching(astroid2):
-                astroid2.delete()
-                laser.delete()
-        if player.is_touching(astroid2):
-            gameoption = 0
-            newly1 = 0
-            game.set_score(0)
-            game.pause()
-            asteroid.delete()
-            astroid2.delete()
-            player.delete()
-            basic.clear_screen()
-        else:
-            astroid2.delete()
-            game.add_score(1)
-basic.forever(on_forever6)
-
-def on_forever7():
-    global asteroid, gameoption, newly1
-    if gameoption == 24:
-        asteroid = game.create_sprite(Math.random_range(0, 5), 0)
-        while asteroid.get(LedSpriteProperty.Y) < 4:
-            asteroid.change(LedSpriteProperty.Y, 1)
-            basic.pause(delay)
-            if laser != None and laser.is_touching(asteroid):
-                asteroid.delete()
-                laser.delete()
-        if player.is_touching(asteroid):
-            gameoption = 0
-            newly1 = 0
-            game.set_score(0)
-            game.pause()
-            asteroid.delete()
-            astroid2.delete()
-            player.delete()
-            basic.clear_screen()
-        else:
-            asteroid.delete()
-            game.add_score(1)
-    elif gameoption == 12:
-        asteroid = game.create_sprite(Math.random_range(0, 5), 0)
-        while asteroid.get(LedSpriteProperty.Y) < 4:
-            asteroid.change(LedSpriteProperty.Y, 1)
-            basic.pause(delay)
-            if laser != None and laser.is_touching(asteroid):
-                asteroid.delete()
-                laser.delete()
-        if player.is_touching(asteroid):
-            gameoption = 1
-            newly1 = 0
-            game.set_score(0)
-            game.pause()
-            asteroid.delete()
-            astroid2.delete()
-            player.delete()
-            basic.clear_screen()
-        else:
-            asteroid.delete()
-            game.add_score(1)
-basic.forever(on_forever7)
+basic.forever(on_forever9)
 
 def on_in_background():
     global runningstatus
@@ -354,8 +394,3 @@ def on_in_background():
         runningstatus = 1
         basic.pause(2000)
 control.in_background(on_in_background)
-
-def on_every_interval2():
-    global hour
-    hour += 1
-loops.every_interval(3600000, on_every_interval2)
--- main.ts
+++ main.ts
@@ -11,10 +11,6 @@ input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
         basic.clearScreen()
     }
 })
-bluetooth.onBluetoothConnected(function () {
-    serial.writeLine("")
-    serial.writeLine("Bluetooth is connected")
-})
 input.onButtonPressed(Button.A, function () {
     if (noDo == 0) {
         if (gameoption == 24) {
@@ -27,6 +23,14 @@ input.onButtonPressed(Button.A, function () {
     }
 })
 function DoLoad () {
+    let microVersion = control.hardwareVersion()
+if (microVersion == "1") {
+        basic.showString("Incompatible MicroBit!")
+        basic.pause(4000)
+        control.panic(668)
+    }
+    bluetooth.startUartService()
+    bluetooth.setTransmitPower(8)
     serial.setBaudRate(BaudRate.BaudRate14400)
     music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.LoopingInBackground)
     noDo = 1
@@ -40,6 +44,7 @@ function DoLoad () {
         # . . . .
         # . . . .
         `)
+    serial.writeLine("Starting bluetooth..")
     basic.pause(randdiv / 2.5)
     basic.showLeds(`
         # # . . .
@@ -56,6 +61,9 @@ function DoLoad () {
         # # # . .
         # # # . .
         `)
+    serial.writeLine("Loading Modules..")
+    basic.pause(900)
+    serial.writeLine("ModuleLoad: Done")
     basic.pause(randdiv / 2.5)
     basic.showLeds(`
         # # # # .
@@ -72,8 +80,10 @@ function DoLoad () {
         # # # # #
         # # # # #
         `)
+    serial.writeLine("Setting Transmit Power..")
     basic.showIcon(IconNames.Yes)
     mkae1 = randdiv / 2.5 * 4
+    serial.writeLine("Finalizing Boot..")
     serial.writeLine("Took " + ("" + mkae1) + " ms to load")
     serial.writeLine("----------------------------------------------------------")
     serial.writeLine("MicroWatch 1.2/Beta Running on Micro:bit \"" + control.deviceName() + "\"")
@@ -148,17 +158,22 @@ buttonClicks.onButtonHeld(buttonClicks.AorB.A, function () {
 })
 let runningstatus = 0
 let xAcceleration = 0
+let BleBSubNum = 0
+let BleB = ""
+let BleASubNum = 0
+let bleA = ""
 let scores = ""
 let time = ""
+let minute = 0
 let mkae1 = 0
 let randdiv = 0
 let randNum1 = 0
+let hour = 0
 let noexecute = 0
 let newly1 = 0
 let gameoption = 0
 let astroid2: game.LedSprite = null
 let noDo = 0
-let hour = 0
 let player : game.LedSprite = null
 let asteroid : game.LedSprite = null
 let laser : game.LedSprite = null
@@ -166,188 +181,14 @@ let second = 0
 let noDo2 = 0
 let randNum12 = 0
 let randdiv2 = 0
-let minute = 0
-serial.redirectToUSB()
 DoLoad()
 let delay = 500
-bluetooth.startUartService()
 loops.everyInterval(60000, function () {
     minute += 1
 })
 basic.forever(function () {
-    if (gameoption == 24) {
-        astroid2 = game.createSprite(Math.randomRange(0, 5), 0)
-        while (astroid2.get(LedSpriteProperty.Y) < 4) {
-            astroid2.change(LedSpriteProperty.Y, 1)
-            basic.pause(delay)
-            if (laser != null && laser.isTouching(astroid2)) {
-                astroid2.delete()
-                laser.delete()
-            }
-        }
-        if (player.isTouching(astroid2)) {
-            gameoption = 0
-            newly1 = 0
-            game.setScore(0)
-            game.pause()
-            asteroid.delete()
-            astroid2.delete()
-            player.delete()
-            basic.clearScreen()
-        } else {
-            astroid2.delete()
-            game.addScore(1)
-        }
-    } else if (gameoption == 12) {
-        astroid2 = game.createSprite(Math.randomRange(0, 5), 0)
-        while (astroid2.get(LedSpriteProperty.Y) < 4) {
-            astroid2.change(LedSpriteProperty.Y, 1)
-            basic.pause(delay)
-            if (laser != null && laser.isTouching(astroid2)) {
-                astroid2.delete()
-                laser.delete()
-            }
-        }
-        if (player.isTouching(astroid2)) {
-            gameoption = 0
-            newly1 = 0
-            game.setScore(0)
-            game.pause()
-            asteroid.delete()
-            astroid2.delete()
-            player.delete()
-            basic.clearScreen()
-        } else {
-            astroid2.delete()
-            game.addScore(1)
-        }
-    }
-})
-basic.forever(function () {
-    while (true) {
-        for (let i = 1; i <= 24; i++) {
-            if (bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine)) == `Hour=${i}`) {
-                hour = i;
-            }
-        }
-    }
-})
-basic.forever(function () {
-    while (true) {
-        for (let j = 1; j <= 60; j++) {
-            if (bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine)) == `Min=${j}`) {
-                minute = j;
-            }
-        }
-    }
-})
-basic.forever(function () {
-    if (gameoption == 24) {
-        asteroid = game.createSprite(Math.randomRange(0, 5), 0)
-        while (asteroid.get(LedSpriteProperty.Y) < 4) {
-            asteroid.change(LedSpriteProperty.Y, 1)
-            basic.pause(delay)
-            if (laser != null && laser.isTouching(asteroid)) {
-                asteroid.delete()
-                laser.delete()
-            }
-        }
-        if (player.isTouching(asteroid)) {
-            gameoption = 0
-            newly1 = 0
-            game.setScore(0)
-            game.pause()
-            asteroid.delete()
-            astroid2.delete()
-            player.delete()
-            basic.clearScreen()
-        } else {
-            asteroid.delete()
-            game.addScore(1)
-        }
-    } else if (gameoption == 12) {
-        asteroid = game.createSprite(Math.randomRange(0, 5), 0)
-        while (asteroid.get(LedSpriteProperty.Y) < 4) {
-            asteroid.change(LedSpriteProperty.Y, 1)
-            basic.pause(delay)
-            if (laser != null && laser.isTouching(asteroid)) {
-                asteroid.delete()
-                laser.delete()
-            }
-        }
-        if (player.isTouching(asteroid)) {
-            gameoption = 1
-            newly1 = 0
-            game.setScore(0)
-            game.pause()
-            asteroid.delete()
-            astroid2.delete()
-            player.delete()
-            basic.clearScreen()
-        } else {
-            asteroid.delete()
-            game.addScore(1)
-        }
-    }
-})
-basic.forever(function () {
-    if (noDo == 0) {
-        if (gameoption == 24) {
-            input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
-            
-            laser = game.createSprite(player.get(LedSpriteProperty.X), player.get(LedSpriteProperty.Y))
-            while (laser.get(LedSpriteProperty.Y) > 0) {
-                laser.change(LedSpriteProperty.Y, -1)
-                basic.pause(50)
-                if (laser != null && laser.isTouching(asteroid)) {
-                    asteroid.delete()
-                    laser.delete()
-                }
-                
-            }
-            laser.delete()
-        })
-        } else if (gameoption == 12) {
-            input.onButtonPressed(Button.AB, function on_button_pressed_ab2() {
-            
-            laser = game.createSprite(player.get(LedSpriteProperty.X), player.get(LedSpriteProperty.Y))
-            while (laser.get(LedSpriteProperty.Y) > 0) {
-                laser.change(LedSpriteProperty.Y, -1)
-                basic.pause(50)
-                if (laser != null && laser.isTouching(asteroid)) {
-                    asteroid.delete()
-                    laser.delete()
-                }
-                
-            }
-            laser.delete()
-        })
-        } else {
-            noexecute = 8
-        }
-    }
-})
-basic.forever(function () {
-    while (true) {
-        for (let k = 1; k <= 60; k++) {
-            if (serial.readUntil(serial.delimiters(Delimiters.NewLine)) == `Min=${k}`) {
-                minute = k;
-            }
-        }
-    }
-})
-basic.forever(function () {
     if (noDo == 0) {
         scores = "Score:" + ("" + game.score())
-    }
-})
-basic.forever(function () {
-    while (true) {
-        for (let l = 1; l <= 24; l++) {
-            if (serial.readUntil(serial.delimiters(Delimiters.NewLine)) == `Hour=${l}`) {
-                hour = l;
-            }
-        }
     }
 })
 basic.forever(function () {
@@ -356,8 +197,36 @@ basic.forever(function () {
             hour = 0
         } else if (minute == 60) {
             minute = 0
+            hour += 1
         }
-        time = "" + hour + ":" + ("" + minute)
+    }
+    time = "" + hour + ":" + ("" + minute)
+})
+basic.forever(function () {
+    bleA = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
+    BleASubNum = parseFloat(bleA.substr(5, 8))
+    if (bleA.includes("Hour=")) {
+        if (BleASubNum < 25) {
+            hour = BleASubNum
+            serial.writeLine("The Hour is set to " + hour + " Via Bluetooth serial Input.")
+        }
+    }
+})
+basic.forever(function () {
+    BleB = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
+    BleBSubNum = parseFloat(BleB.substr(4, 7))
+    if (BleB.includes("Min=")) {
+        if (BleBSubNum < 61) {
+            minute = BleBSubNum
+            serial.writeLine("The Minute is set to " + minute + " Via Bluetooth serial Input.")
+        }
+    }
+})
+basic.forever(function () {
+    if (noDo == 0) {
+        if (gameoption == 24) {
+            player = game.createSprite(2, 4)
+        }
     }
 })
 basic.forever(function () {
@@ -383,10 +252,144 @@ basic.forever(function () {
         }
     }
 })
+// asteroid movement
 basic.forever(function () {
     if (noDo == 0) {
         if (gameoption == 24) {
-            player = game.createSprite(2, 4)
+            astroid2 = game.createSprite(Math.randomRange(0, 5), 0)
+            while (astroid2.get(LedSpriteProperty.Y) < 4) {
+                astroid2.change(LedSpriteProperty.Y, 1)
+                basic.pause(delay)
+                if (laser != null && laser.isTouching(astroid2)) {
+                    astroid2.delete()
+                    laser.delete()
+                }
+            }
+            if (player.isTouching(astroid2)) {
+                gameoption = 0
+                newly1 = 0
+                game.setScore(0)
+                game.pause()
+                asteroid.delete()
+                astroid2.delete()
+                player.delete()
+                basic.clearScreen()
+            } else {
+                astroid2.delete()
+                game.addScore(1)
+            }
+        } else if (gameoption == 12) {
+            astroid2 = game.createSprite(Math.randomRange(0, 5), 0)
+            while (astroid2.get(LedSpriteProperty.Y) < 4) {
+                astroid2.change(LedSpriteProperty.Y, 1)
+                basic.pause(delay)
+                if (laser != null && laser.isTouching(astroid2)) {
+                    astroid2.delete()
+                    laser.delete()
+                }
+            }
+            if (player.isTouching(astroid2)) {
+                gameoption = 0
+                newly1 = 0
+                game.setScore(0)
+                game.pause()
+                asteroid.delete()
+                astroid2.delete()
+                player.delete()
+                basic.clearScreen()
+            } else {
+                astroid2.delete()
+                game.addScore(1)
+            }
+        }
+    }
+})
+basic.forever(function () {
+    if (noDo == 0) {
+        if (gameoption == 24) {
+            asteroid = game.createSprite(Math.randomRange(0, 5), 0)
+            while (asteroid.get(LedSpriteProperty.Y) < 4) {
+                asteroid.change(LedSpriteProperty.Y, 1)
+                basic.pause(delay)
+                if (laser != null && laser.isTouching(asteroid)) {
+                    asteroid.delete()
+                    laser.delete()
+                }
+            }
+            if (player.isTouching(asteroid)) {
+                gameoption = 0
+                newly1 = 0
+                game.setScore(0)
+                game.pause()
+                asteroid.delete()
+                astroid2.delete()
+                player.delete()
+                basic.clearScreen()
+            } else {
+                asteroid.delete()
+                game.addScore(1)
+            }
+        } else if (gameoption == 12) {
+            asteroid = game.createSprite(Math.randomRange(0, 5), 0)
+            while (asteroid.get(LedSpriteProperty.Y) < 4) {
+                asteroid.change(LedSpriteProperty.Y, 1)
+                basic.pause(delay)
+                if (laser != null && laser.isTouching(asteroid)) {
+                    asteroid.delete()
+                    laser.delete()
+                }
+            }
+            if (player.isTouching(asteroid)) {
+                gameoption = 1
+                newly1 = 0
+                game.setScore(0)
+                game.pause()
+                asteroid.delete()
+                astroid2.delete()
+                player.delete()
+                basic.clearScreen()
+            } else {
+                asteroid.delete()
+                game.addScore(1)
+            }
+        }
+    }
+})
+// firing lasers
+basic.forever(function () {
+    if (noDo == 0) {
+        if (gameoption == 24) {
+            input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+                
+                laser = game.createSprite(player.get(LedSpriteProperty.X), player.get(LedSpriteProperty.Y))
+                while (laser.get(LedSpriteProperty.Y) > 0) {
+                    laser.change(LedSpriteProperty.Y, -1)
+                    basic.pause(50)
+                    if (laser != null && laser.isTouching(asteroid)) {
+                        asteroid.delete()
+                        laser.delete()
+                    }
+                    
+                }
+                laser.delete()
+            })
+        } else if (gameoption == 12) {
+            input.onButtonPressed(Button.AB, function on_button_pressed_ab2() {
+                
+                laser = game.createSprite(player.get(LedSpriteProperty.X), player.get(LedSpriteProperty.Y))
+                while (laser.get(LedSpriteProperty.Y) > 0) {
+                    laser.change(LedSpriteProperty.Y, -1)
+                    basic.pause(50)
+                    if (laser != null && laser.isTouching(asteroid)) {
+                        asteroid.delete()
+                        laser.delete()
+                    }
+                    
+                }
+                laser.delete()
+            })
+        } else {
+            noexecute = 8
         }
     }
 })
@@ -395,7 +398,4 @@ control.inBackground(function () {
         runningstatus = 1
         basic.pause(2000)
     }
-})
-loops.everyInterval(3600000, function () {
-    hour += 1
 })
